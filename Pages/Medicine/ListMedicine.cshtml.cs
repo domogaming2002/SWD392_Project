9f47eb5d6e94eaa09e03df5cfb0734d67ae093cd
MinhTD <aigiabeomap@gmail.com>
2023-07-16 19:33:01 +0700

    Add project files.

---
+++ Pages/Medicine/ListMedicine.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWD392_Project.Pages.Medicine
+{
+    public class ListMedicineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
